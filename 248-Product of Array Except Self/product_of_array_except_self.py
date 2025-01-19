cf4f9a532450755f22f427bdac73052788944061
Yousaf-AJan <169179806+Yousaf-AJan@users.noreply.github.com>
2025-01-18 20:49:19 -0500

    Solved Product Of Array Except Self

---
+++ 248-Product of Array Except Self/product_of_array_except_self.py
@@ -0,0 +1,18 @@
+
+#Time Complexity: O(n)
+#Space Complexity: O(1)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = [1] * (len(nums))
+
+        pre = 1
+        for i in range(len(nums)):
+            answer[i] = pre
+            pre *= nums[i]
+
+        post = 1
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] *= post
+            post *= nums[i]
+        
+        return answer        
